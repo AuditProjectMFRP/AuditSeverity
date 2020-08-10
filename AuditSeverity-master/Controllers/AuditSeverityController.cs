5fb7fb343077b1558140371b45bffbbe6a8bd6da
AuditProjectMFRP <projectaudit2020@gmail.com>
2020-08-10 22:25:41 +0530

    initial

---
+++ AuditSeverity-master/Controllers/AuditSeverityController.cs
@@ -0,0 +1,87 @@
+﻿using Audit_management_portal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AuditSeverityService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditSeverityController : ControllerBase
+    {
+        private readonly log4net.ILog _log4net;
+        public IConfiguration _config;
+        public AuditSeverityController(IConfiguration config)
+        {
+            _config = config;
+            _log4net = log4net.LogManager.GetLogger(typeof(AuditSeverityController));
+        }
+
+        [HttpPost]
+      //  [Authorize]
+        public async Task<ActionResult<AuditResponse>> ProjectExecutionStatus([FromBody] AuditRequest AuditRequest)
+        {
+            if (AuditRequest.ProjectName == null || AuditRequest.ProjectManagerName == null || AuditRequest.AuditDetails.AuditType ==null || AuditRequest.ApplicationOwnerName==null || AuditRequest.AuditDetails.AuditDate==null || AuditRequest.AuditDetails.AuditQuestions==null)
+                return BadRequest("Please Enter All the Values");                 //check all inputs are given or not
+
+            if (!AuditRequest.AuditDetails.AuditType.Equals("Internal",StringComparison.InvariantCultureIgnoreCase) && !AuditRequest.AuditDetails.AuditType.Equals("SOX",StringComparison.InvariantCultureIgnoreCase)) //Incase of Invalid Input
+                return BadRequest("Invalid AuditType Input");                  //check for invalid auditTYpe input
+
+            _log4net.Info("AuditSeverity Post Method For "+ AuditRequest.AuditDetails.AuditType);
+
+            List<AuditBenchmark> ListAuditBenchamark;
+            AuditResponse obj = new AuditResponse();
+            int AcceptableNoValue=0;
+            int CountNo = 0;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage  response =await client.GetAsync(_config["Links:AuditBenchmark"]);      //call benchmark
+                string result = response.Content.ReadAsStringAsync().Result;
+                ListAuditBenchamark = JsonConvert.DeserializeObject<List<AuditBenchmark>>(result);
+
+            }
+              foreach (var item in ListAuditBenchamark)                  //To Store Acceptable "NO" values returned from the benchmark api
+            {
+                if (item.AuditType.Equals(AuditRequest.AuditDetails.AuditType,StringComparison.InvariantCultureIgnoreCase))
+                    AcceptableNoValue = item.BenchmarkNoAnswers;
+            }
+
+            for (int i = 0; i < AuditRequest.AuditDetails.AuditQuestions.Count; i++)                      //calculate number of "NO" entere by user
+            {
+                if (AuditRequest.AuditDetails.AuditQuestions.ElementAt(i).Value.Equals("NO",StringComparison.InvariantCultureIgnoreCase))                
+                    CountNo++;
+            }
+
+            Random r = new Random();
+            obj.AuditId = r.Next(1, 99999);                         //generate random number to assign to AuditId
+
+            if (AuditRequest.AuditDetails.AuditType .Equals("Internal",StringComparison.InvariantCultureIgnoreCase) && CountNo > AcceptableNoValue)         //enter Audit Response values based upon conditions
+            {
+                obj.ProjectExecutionStatus = "RED";
+                obj.RemedialActionDuration = "Action to be taken in 2 weeks";
+            }
+            else if (AuditRequest.AuditDetails.AuditType.Equals("SOX",StringComparison.InvariantCultureIgnoreCase) && CountNo > AcceptableNoValue)
+            {
+                obj.ProjectExecutionStatus = "RED";
+                obj.RemedialActionDuration = "Action to be taken in 1 week";
+            }
+            else 
+            { 
+                obj.ProjectExecutionStatus = "GREEN";
+                obj.RemedialActionDuration = "No action needed";
+            }
+            
+            return Created(String.Empty,obj);              //return Audit Response
+        }
+    }
+}
---
+++ AuditSeverityTest/AuditSeverityTest.cs
@@ -0,0 +1,138 @@
+
+using Audit_management_portal;
+using Audit_management_portal.Models;
+using AuditSeverityService;
+using AuditSeverityService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace AuditSeverityTest
+{
+    public class AuditSeverity
+
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        
+        [Test]
+        public void CheckAuditResponseObject_WhenAuditTypeIsInternal()
+        {
+            //Arrange
+            AuditRequest obj = new AuditRequest();
+            AuditDetails b = new AuditDetails();
+            b.AuditDate = Convert.ToDateTime("2020-08-02");
+            b.AuditType = "Internal";
+            Dictionary<string, string> a = new Dictionary<string, string>();
+            a.Add("Have all Change requests followed SDLC before PROD move? ", "Yes");
+            a.Add("Have all Change requests been approved by the application owner?", "Yes");
+            a.Add("Are all artifacts like CR document, Unit test cases available?", "Yes");
+            a.Add("Is the SIT and UAT sign-off available?", "Yes");
+            a.Add("Is data deletion from the system done with application owner approval?", "Yes");
+            b.AuditQuestions = a;
+            obj.AuditDetails = b;
+            obj.ProjectManagerName = "kunal";
+            obj.ProjectName = "audit";
+            obj.ApplicationOwnerName = "cts";
+
+            Mock<IConfiguration> config = new Mock<IConfiguration>();
+            //Act
+            AuditSeverityController Controller = new AuditSeverityController(config.Object);
+            var auditResponse = Controller.ProjectExecutionStatus(obj);
+            var res = auditResponse.Result;
+            var c = res.Result as CreatedResult;
+            var d = c.Value as AuditResponse;
+
+            //Assert
+            Assert.AreEqual("GREEN", d.ProjectExecutionStatus);
+            Assert.AreEqual("No action needed", d.RemedialActionDuration);
+
+        }
+
+        [Test]
+        public void CheckAuditResponseObject_WhenAuditTypeIsSOX()
+        {
+            //Arrange
+            AuditRequest obj = new AuditRequest();
+            AuditDetails b = new AuditDetails();
+            b.AuditDate = Convert.ToDateTime("2020-08-12");
+            b.AuditType = "SOX";
+            Dictionary<string, string> Question = new Dictionary<string, string>();
+            Question.Add("Have all Change requests followed SDLC before PROD move?", "NO");
+            Question.Add("Have all Change requests been approved by the application owner?", "NO");
+            Question.Add("For a major change, was there a database backup taken before and after PROD move?", "NO");
+            Question.Add("Has the application owner approval obtained while adding a user to the system?", "NO");
+            Question.Add("Is data deletion from the system done with application owner approval?", "NO");
+            b.AuditQuestions = Question;
+            obj.AuditDetails = b;
+            obj.ProjectManagerName = "subhanshu";
+            obj.ProjectName = "audit";
+            obj.ApplicationOwnerName = "cts";
+            Mock<IConfiguration> config = new Mock<IConfiguration>();
+
+            //Act
+            AuditSeverityController Controller = new AuditSeverityController(config.Object);
+            var auditResponse = Controller.ProjectExecutionStatus(obj);
+            var res = auditResponse.Result;
+            var c = res.Result as CreatedResult;
+            var d = c.Value as AuditResponse;
+
+            //Assert
+            Assert.AreEqual("RED", d.ProjectExecutionStatus);
+            Assert.AreEqual("Action to be taken in 1 week", d.RemedialActionDuration);
+        }
+
+    
+        [Test]
+        public void GetAuditCheckListQuestions_WhenInvalidInputIsPassed_ReturnsBadRequest()
+        {
+            //Arrange
+            AuditRequest obj = new AuditRequest();
+            AuditDetails b = new AuditDetails();
+            b.AuditType = "InvalidInput"; ;
+            b.AuditDate = Convert.ToDateTime("2020-08-12");
+            Dictionary<string, string> Question = new Dictionary<string, string>();
+            Question.Add("Have all Change requests followed SDLC before PROD move?", "NO");
+            Question.Add("Have all Change requests been approved by the application owner?", "NO");
+            Question.Add("For a major change, was there a database backup taken before and after PROD move?", "NO");
+            Question.Add("Has the application owner approval obtained while adding a user to the system?", "NO");
+            Question.Add("Is data deletion from the system done with application owner approval?", "NO");
+            b.AuditQuestions = Question;
+            obj.AuditDetails = b;
+            obj.ProjectManagerName = "subhanshu";
+            obj.ProjectName = "audit";
+            obj.ApplicationOwnerName = "cts";
+            Mock<IConfiguration> config = new Mock<IConfiguration>();
+            //Act
+            AuditSeverityController Controller = new AuditSeverityController(config.Object);
+            var ControllerOutput = Controller.ProjectExecutionStatus(obj);
+            var result = ControllerOutput.Result;
+            var a = result.Result as BadRequestObjectResult;
+
+
+
+            //Assert     
+            Assert.AreEqual("Invalid AuditType Input", a.Value);
+        }
+
+        [Test]
+        public void GetAuditCheckListQuestions_WhenEmptyObjectIsPassed_ReturnsBadRequest()
+        {
+            Mock<IConfiguration> config = new Mock<IConfiguration>();
+            AuditRequest obj = new AuditRequest();
+            AuditSeverityController Controller = new AuditSeverityController(config.Object);
+            var ControllerOutput = Controller.ProjectExecutionStatus(obj);
+            var result = ControllerOutput.Result;
+            var a=result.Result as BadRequestObjectResult;
+
+            Assert.AreEqual("Please Enter All the Values", a.Value);
+        }
+
+
+    }
+}
